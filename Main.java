d114220b4aa9a2e80992d9c64ba660123469eb06
kkapitvla <161339763+kkapitvla@users.noreply.github.com>
2024-04-08 15:20:17 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     {
         System.out.println("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 15; i++) {
             System.out.println("i = " + i);
         }
     }
